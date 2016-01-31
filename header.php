8f9e4c3b85c71157d05a5894ef21a0f5b1e0ccb7
U-ADAM-PC\adam <gemailadam@gmail.com>
2016-01-31 23:10:28 +0200

    20160131 part 6

--- header.php
+++ header.php
@@ -1,3 +1,10 @@
 <div class="qwe-nav" >
 	<?php wp_nav_menu(array('theme_location'=>'header')); ?>
 </div>
+<div class="qwe-site-title">
+	<a href="<?php the_permalink();?>"><?php bloginfo('title' ); ?></a>
+	<a href="<?php  the_permalink();?>"><?php bloginfo('description' );?></a>
+</div>
+<div class="qwe-header-img">
+	<img src="<?php echo get_template_directory_uri(); ?>/images/light-love-clouds-river.jpg" alt="">
+</div>
